1f086df4e26b44f1c822ad7f9dbbd1bfddce7f77
WillCup <willcup@163.com>
2017-10-23 17:20:27 +0800

    another

--- bin/start.sh
+++ bin/start.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
-java -cp will_sync-1.0.2.jar \
- -DCANAL_SERVER_IP=${CANAL_SERVER_IP} \
- -DCANAL_SERVER_PORT=${CANAL_SERVER_PORT} \
- -DDESTINATION=${DESTINATION} \
- -DFILTER=${FILTER} \
- -DKAFKA_SERVERS=${KAFKA_SERVERS} \
- com.alibaba.otter.canal.process.One
+
+sed -e "s/{CANAL_SERVER_IP}/${CANAL_SERVER_IP}/g" \
+    -e "s/{CANAL_SERVER_PORT}/${CANAL_SERVER_PORT}/g" \
+    -e "s/{DESTINATION}/${DESTINATION}/g" \
+    -e "s/{FILTER}/${FILTER}/g" \
+    -e "s/{KAFKA_SERVERS}/${KAFKA_SERVERS}/g" \
+    -e "s/{KAFKA_TOPIC}/${KAFKA_TOPIC}/g" \
+    -e "s/{USER_NAME}/${USER_NAME}/g" \
+    -e "s/{PASSWORD}/${PASSWORD}/g" \
+    /SysConfig.properties_template > /SysConfig.properties
+java -cp will_sync-1.0.2.jar com.alibaba.otter.canal.process.One
